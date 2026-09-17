3c0224369bdfbce687adeae829a44a764e3c11c3
Lesha Light <senodin34@gmail.com>
2026-09-17 16:59:29 +0300

    +++

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { arrData } from './data.tsx'
-
+// const arrData = null;
 
 export function App() {
 
+	if(arrData === null){
+		return <span>Loading...</span>
+	}
+	if(arrData.length === 0){
+		return <span>No Data</span>
+	}
+
   return (
     <div>
-		{arrData.map((arrData)=>{
+		{arrData.map((currentData)=>{
 			return(
-				<div key={arrData.id} onClick={()=>{console.log(arrData.completed)}}>{arrData.title}</div>
+				<div key={currentData.id} onClick={()=>{console.log(currentData.completed)}}>{currentData.title}</div>
 			)
 		})}
     </div>
   )
+
+
 }
 
